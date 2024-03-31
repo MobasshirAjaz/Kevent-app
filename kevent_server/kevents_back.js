a36488a941317965910cf5146cd05b40288c1019
Mobasshir Ajaz <mobasshirajaz@gmail.com>
2024-03-31 20:58:30 +0530

    Minor changes

--- kevent_server/kevents_back.js
+++ kevent_server/kevents_back.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-// MySQL connection settings
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'aman',
@@ -16,11 +16,10 @@ connection.connect(error => {
   console.log('Successfully connected to the database.');
 });
 
-// Endpoint to get data from MySQL and send to Android app
+
 app.get('/data', (req, res) => {
   connection.query('SELECT * FROM events', (error, results) => {
     if (error) throw error;
-    // Send data as JSON
     res.json(results);
   });
 });
